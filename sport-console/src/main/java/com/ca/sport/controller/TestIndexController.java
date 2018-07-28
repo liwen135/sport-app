50fc469fe465759a7d4a269d22b158be1ca413e3
liwen135 <1396968962@qq.com>
2018-07-29 00:26:47 +0800

    增加dubbo服务,需要调试

--- sport-console/src/main/java/com/ca/sport/controller/TestIndexController.java
+++ sport-console/src/main/java/com/ca/sport/controller/TestIndexController.java
@@ -1,14 +1,26 @@
 package com.ca.sport.controller;
 
+import com.ca.sport.bean.TestApp;
+import com.ca.sport.service.test.TestAppService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
 
 @Controller
 @RequestMapping("/")
 public class TestIndexController {
 
+    @Resource
+    private TestAppService testAppService;
+
+
     @RequestMapping("/index")
-    public String gotoIndex() {
+    public String gotoIndex(Model model) {
+        TestApp testApp = testAppService.selectById(1L);
+        model.addAttribute("name", testApp.getName());
+        model.addAttribute("age", testApp.getAge());
         return "index";
     }
 }
--- sport-service-product/src/main/java/com/ca/sport/service/test/TestAppServiceImpl.java
+++ sport-service-product/src/main/java/com/ca/sport/service/test/TestAppServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
-@Service
+@Service("testAppService")
 public class TestAppServiceImpl implements TestAppService {
     @Resource
     private TestAppDao testAppDao;
