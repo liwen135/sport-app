bb6191c3c73c07527ece9577caa63024a9297e12
liwen135 <1396968962@qq.com>
2018-07-29 09:24:56 +0800

    修改日志

--- sport-console/src/main/java/com/ca/sport/controller/TestIndexController.java
+++ sport-console/src/main/java/com/ca/sport/controller/TestIndexController.java
@@ -2,6 +2,8 @@ package com.ca.sport.controller;
 
 import com.ca.sport.bean.TestApp;
 import com.ca.sport.service.test.TestAppService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +14,18 @@ import javax.annotation.Resource;
 @RequestMapping("/")
 public class TestIndexController {
 
-    @Resource
+    private Logger logger = LoggerFactory.getLogger(TestIndexController.class);
+    //    @Resource
     private TestAppService testAppService;
 
 
     @RequestMapping("/index")
     public String gotoIndex(Model model) {
-        TestApp testApp = testAppService.selectById(1L);
-        model.addAttribute("name", testApp.getName());
-        model.addAttribute("age", testApp.getAge());
+//        TestApp testApp = testAppService.selectById(1L);
+//        model.addAttribute("name", testApp.getName());
+//        model.addAttribute("age", testApp.getAge());
+        model.addAttribute("test", "testApp");
+        logger.debug("test controller");
         return "index";
     }
 }
--- sport-console/src/main/webapp/WEB-INF/console/index.ftl
+++ sport-console/src/main/webapp/WEB-INF/console/index.ftl
@@ -1,1 +1,16 @@
+<html>
+<head>
+
+</head>
+<body>
+
 <h1>console-index</h1>
+name:${name!}</br>
+name:${age!}</br>
+test:${test!}</br>
+
+</body>
+
+
+</html>
+
