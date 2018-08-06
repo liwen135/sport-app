0dc25cb54bd70a42793c9ee76763bf759ad77fb8
liwen135 <1396968962@qq.com>
2018-08-06 23:54:12 +0800

    更新redis

--- sport-service-product/src/test/java/com/ca/sport/test/redis/TestRedis.java
+++ sport-service-product/src/test/java/com/ca/sport/test/redis/TestRedis.java
@@ -1,14 +1,33 @@
 package com.ca.sport.test.redis;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import redis.clients.jedis.Jedis;
 
+import javax.annotation.Resource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring-config.xml")
 public class TestRedis {
 
+    @Resource
+    private Jedis jedis;
+
     @Test
-    public void test01(){
-        Jedis jedis = new Jedis("192.168.47.100", 6379);
+    public void test01() {
+        Jedis jedis = new Jedis("192.168.198.134", 6379);
         String pno = jedis.get("pno");
         System.out.println(pno);
+    }
+
+    @Test
+    public void test02() {
+        String pno = jedis.get("pno");
+        System.out.println(pno);
+        Long incr = jedis.incrBy("pno", 5);
+        System.out.println(incr);
+        System.out.println(jedis.get("pno"));
     }
 }
